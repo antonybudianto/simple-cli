9817b79687780bec21874c0be051f243605df965
Antony Budianto <antonybudianto@gmail.com>
2023-08-10 09:56:27 +0700

    add yargs

--- cli.js
+++ cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log("Hello world!", process.argv);
+// console.log(process.argv);
+const argv = require("yargs/yargs")(process.argv.slice(2)).argv;
+
+console.log("out:", argv.x + argv.y);
